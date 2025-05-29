3b3aa32fcc15a2c78fc28ececbe589aebc841a1b
mohamedkh001 <126171319+mohamedkh001@users.noreply.github.com>
2025-05-29 15:46:23 +0300

    Update route_tables.tf

--- terraform/route_tables.tf
+++ terraform/route_tables.tf
@@ -20,6 +20,11 @@ resource "aws_route_table_association" "public_assoc" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
   tags = {
     Name = "${var.project_name}-${var.environment}-private-rt"
   }
