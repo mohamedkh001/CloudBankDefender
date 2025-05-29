1d498c380b0ed9eaf610e34737b06f9aa1299798
mohamedkh001 <126171319+mohamedkh001@users.noreply.github.com>
2025-05-29 15:19:18 +0300

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,35 +1,46 @@
 resource "aws_iam_role" "ec2_role" {
-  name = "${var.project_name}-${var.environment}-ec2-role"
+  name = var.ec2_role_name
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
       }
-    }]
+    ]
   })
+
+  tags = {
+    Name        = var.ec2_role_name
+    Environment = var.environment
+    Project     = var.project_name
+  }
 }
 
 resource "aws_iam_policy" "ec2_policy" {
-  name        = "${var.project_name}-${var.environment}-ec2-policy"
-  description = "Policy for EC2 instances to access S3 buckets"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = [
-        "s3:GetObject",
-        "s3:PutObject"
-      ]
-      Effect   = "Allow"
-      Resource = "arn:aws:s3:::example-bucket/*"
-    }]
+  name        = var.ec2_policy_name
+  description = "Policy allowing EC2 to access specific S3 resources"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = var.s3_bucket_arn
+      }
+    ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "attach_ec2_policy" {
+resource "aws_iam_role_policy_attachment" "attach_policy" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = aws_iam_policy.ec2_policy.arn
 }
