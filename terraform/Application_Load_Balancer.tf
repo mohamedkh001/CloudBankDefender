80c3441fe7eeb83a0e18a78aba8e49064ad28666
mohamedkh001 <126171319+mohamedkh001@users.noreply.github.com>
2025-05-29 15:32:42 +0300

    app load balancer

---
+++ terraform/Application_Load_Balancer.tf
@@ -0,0 +1,50 @@
+resource "aws_lb" "app_lb" {
+  name               = "cloudbank-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+
+  tags = {
+    Name        = "CloudBank-ALB"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+resource "aws_lb_target_group" "web_tg" {
+  name     = "cloudbank-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "Web-TG"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web_instance" {
+  target_group_arn = aws_lb_target_group.web_tg.arn
+  target_id        = aws_instance.web_server.id
+  port             = 80
+}
